7bd1a920bff6682f863f1e495991c696411e0dca
Ramanathan Vadivel <ramanathan.vadivel@npcompete.com>
2021-03-13 14:50:02 +0530

    Initial Commit

---
+++ Notes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Mac HD on 13/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
